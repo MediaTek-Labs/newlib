65b81ba4d48aea0f1b4619a498f362514ab1d91a
Dragan Mladjenovic <vend_draganm001@mediatek.com>
2021-02-09 17:11:40 +0100

    Enable USE_MALLOC_DTOA on nanomips
    	newlib/
    		* libc/include/sys/config.h: Define USE_MALLOC_DTOA
    		under [defined(__nanomips__)]

--- newlib/libc/include/sys/config.h
+++ newlib/libc/include/sys/config.h
@@ -8,6 +8,10 @@
 #define MALLOC_ALIGNMENT 16
 #endif
 
+#if defined(__nanomips__)
+#define USE_MALLOC_DTOA
+#endif
+
 /* exceptions first */
 #if defined(__H8500__) || defined(__W65__)
 #define __SMALL_BITFIELDS
